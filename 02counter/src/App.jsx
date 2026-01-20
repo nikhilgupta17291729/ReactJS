dfcf2669106bdc8fe04c2440411ee2b88d962679
Nikhil Gupta <nikhil@example.com>
2026-01-20 15:59:58 +0530

    Counter App Implementation

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,31 @@
+import{ useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    if (counter >= 20) {
+          return counter
+    }
+      setCounter(counter + 1);
+  }
+
+  const removeValue = () => { 
+    if (counter <= 0) { 
+      return counter
+    }
+    setCounter(counter - 1)
+  }
+  return (
+    <>
+      <h1>Chai Aur react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add Value {counter}</button><br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  )
+}
+
+export default App
