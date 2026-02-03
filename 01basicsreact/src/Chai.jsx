1551171dcea7508e4e9f49d8b7dc550a7bf12685
Nikhil Gupta <nikhil@example.com>
2026-02-03 13:45:59 +0530

    Enhances heading with background and padding
    Improves visual emphasis and readability by adding a light blue background and padding to the section heading.

--- 01basicsreact/src/Chai.jsx
+++ 01basicsreact/src/Chai.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Chai = () => {
   return (
     <div>
-      <h3>Welcome to React</h3>
+      <h3 style={{backgroundColor:"lightblue",padding:"10px"}}>Welcome to React</h3>
     </div>
   )
 }
